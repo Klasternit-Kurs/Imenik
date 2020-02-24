146172e266ca96428a60c9cac5b44f01ce99808c
Dragan Pepic <d.pepic@hotmail.com>
2020-02-24 19:57:56 +0100

    Par dodataka

--- Program.cs
+++ Program.cs
@@ -24,11 +24,21 @@ namespace Aplikacija
 					case '1':
 						UnosOsobe();
 						break;
+					case '2':
+						Izmena();
+						break;
+					case '3':
+						Brisanje();
+						break;
 					case '4':
 						Console.WriteLine();
-						foreach ( Osoba o in Osoba.Osobe)
+						/*foreach ( Osoba o in Osoba.Osobe)
 						{
-							Console.WriteLine($"{o.ime} {o.prezime} -- {o.broj}");
+							Console.WriteLine(o); //isto kao o.ToString()
+						}*/
+						for (int indeks = 0; indeks < Osoba.Osobe.Count; indeks++)
+						{
+							Console.WriteLine($"{indeks+1}. -- {Osoba.Osobe[indeks]}");
 						}
 						break;
 					default:
@@ -39,8 +49,49 @@ namespace Aplikacija
 			} while (unos != '5');
 		}
 
+		static void Izmena()
+		{
+			Console.Write("\nUnesite broj telefona: ");
+			string br = Console.ReadLine();
+
+			if (!Osoba.ProveriTel(br, out Osoba o))
+			{
+				Console.Write("\nUnesite ime i prezime: ");
+				string[] imeIprezime = Console.ReadLine().Split(' ');
+				//"Pera Peric" ---> ["Pera", "Peric"]
+				if (imeIprezime.Length != 2)
+				{
+					Console.WriteLine("Greska u unosu!");
+					return;
+				}
+				o.ime = imeIprezime[0];
+				o.prezime = imeIprezime[1];
+			}
+			else
+			{
+				Console.WriteLine("Nema tog broja :(");
+			}
+		}
+
+		static void Brisanje()
+		{
+			//TODO Napraviti brisanje da radi po indeksima
+			//Korisno bi bilo koristiti for petlju standardnu da mozete da 
+			//ispisete indekse, i onda koristite Osoba.Osobe.RemoveAt(indeks) :) 
+			Console.Write("\nUnesite broj telefona: ");
+			string br = Console.ReadLine();
+
+			if (!Osoba.ProveriTel(br, out Osoba o))
+			{
+				Osoba.Osobe.Remove(o);
+			} else
+			{
+				Console.WriteLine("Nema tog broja :(");
+			}
+		}
+
 		static void UnosOsobe()
-		{ //Za domaci, ova metoda u do while, i nema izlaska dok se ne unese
+		{ //TODO Za domaci, ova metoda u do while, i nema izlaska dok se ne unese
 			//sve tacno :) 
 			Console.Write("\nUnesite ime i prezime: ");
 			string[] imeIprezime = Console.ReadLine().Split(' ');
@@ -50,15 +101,21 @@ namespace Aplikacija
 				Console.WriteLine("Greska u unosu!");
 				return;
 			}
-			Console.WriteLine("Unesite broj telefona:");
-			//Za domaci: broj telefona mora da se unese u formatu gde ima
+			Console.Write("Unesite broj telefona:");
+			//TODO Za domaci: broj telefona mora da se unese u formatu gde ima
 			//   / za pozivni i - izmedju dve grupe brojeva, proveriti to :) 
 			string broj = Console.ReadLine();
 
 
-			//Ne treba nam varijabla jer konstruktor automatski smesta 
-			//osobu u list :) 
-			new Osoba(imeIprezime[0], imeIprezime[1], broj);
+			if (Osoba.ProveriTel(broj, out _))
+			{
+				//Ne treba nam varijabla jer konstruktor automatski smesta 
+				//osobu u list :) 
+				new Osoba(imeIprezime[0], imeIprezime[1], broj);
+			} else
+			{
+				Console.WriteLine("Telefon vec postoji!");
+			}
 		}
 
 		//Primer metode sa out paramterom, kao sto je TryParse
@@ -84,6 +141,20 @@ namespace Aplikacija
 	{
 		static public List<Osoba> Osobe = new List<Osoba>();
 
+		static public bool ProveriTel(string t, out Osoba ob)
+		{
+			foreach(Osoba o in Osobe)
+			{
+				if (o.broj == t)
+				{
+					ob = o;
+					return false;
+				}
+			}
+			ob = null;
+			return true;
+		}
+
 		public string ime;
 		public string prezime;
 		public string broj;
@@ -98,6 +169,11 @@ namespace Aplikacija
 		public Osoba() 
 		{
 			Osobe.Add(this);
+		}
+
+		public override string ToString()
+		{
+			return $"{ime} {prezime} -- {broj}";
 		}
 	}
 }
